a4175d556a68b5888e3d83db0306538528d69983
Srividhya <ss154@illinois.edu>
2026-04-10 18:12:11 -0500

    UI: Update badge background to Illinois Blue

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ function ProfilePhoto({ className }: { className?: string }) {
                 />
             </div>
             {/* Official Illinois Badge */}
-            <div className="absolute -bottom-1 -right-1 w-20 h-20 bg-white rounded-full border-4 border-white flex items-center justify-center shadow-lg overflow-hidden p-2">
+            <div className="absolute -bottom-1 -right-1 w-20 h-20 bg-primary rounded-full border-4 border-white flex items-center justify-center shadow-lg overflow-hidden p-2">
                 <div className="relative w-full h-full">
                     <Image
                         src="/Illinois_logo_fullcolor_rgb.png"
