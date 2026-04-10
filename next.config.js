d39673631e082b321d00b47d281f357007ecec3d
Srividhya <ss154@illinois.edu>
2026-04-10 15:16:02 -0500

    Configure robust static export

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 // For GitHub Project Pages, set repo name and uncomment basePath + assetPrefix:
 // const repo = "your-repo-name";
 const nextConfig = {
+    output: "export",
+    trailingSlash: true,
     images: {
         unoptimized: true,
     },
-    // basePath: `/${repo}`,
-    // assetPrefix: `/${repo}/`,
 };
 
 module.exports = nextConfig;
